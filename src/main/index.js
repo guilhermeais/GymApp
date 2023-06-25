174e4e3cd7da0d75d0f096076d3390e6ea6c8228
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-06-25 14:00:23 -0300

    chore: finish expo configuration

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import { registerRootComponent } from 'expo'
-import App from './router/router'
+import {registerRootComponent} from 'expo';
+import App from './router/router';
 
-export default registerRootComponent(App)
+export default registerRootComponent(App);
